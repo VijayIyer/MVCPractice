b254453e32ed054ed27b4a9d404c17eb7bb9487f
VijayIyer <vijayiyer7@gmail.com>
2019-08-25 08:25:39 +0530

    AngularJS adding Library(after loss)

---
+++ MVCPractice/RulesController.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RulesController', RulesController);
+
+    RulesController.$inject = ['$scope']; 
+
+    function RulesController($scope) {
+        $scope.title = 'RulesController';
+
+        activate();
+
+        function activate() { }
+    }
+})();
